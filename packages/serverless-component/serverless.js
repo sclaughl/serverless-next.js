24d7064e9e63ebe02a24b2a19ed7c2dac0a31766
Egor <595980+lone-cloud@users.noreply.github.com>
2020-06-01 11:46:30 -0700

    feat(serverless-component): allow users to set the node.js runtime for the lambda functions (#422)
    Co-authored-by: Egor <philippovegor@gmail.com>

--- packages/serverless-component/serverless.js
+++ packages/serverless-component/serverless.js
@@ -261,6 +261,11 @@ class NextjsComponent extends Component {
         ? inputs.name
         : inputs.name && inputs.name[lambdaType];
 
+    const getLambdaRuntime = lambdaType =>
+      typeof inputs.runtime === "string"
+        ? inputs.runtime
+        : (inputs.runtime && inputs.runtime[lambdaType]) || "nodejs12.x";
+
     if (hasAPIPages) {
       const apiEdgeLambdaInput = {
         description: inputs.description
@@ -277,7 +282,8 @@ class NextjsComponent extends Component {
           }
         },
         memory: getLambdaMemory("apiLambda"),
-        timeout: getLambdaTimeout("apiLambda")
+        timeout: getLambdaTimeout("apiLambda"),
+        runtime: getLambdaRuntime("apiLambda")
       };
       const apiLambdaName = getLambdaName("apiLambda");
       if (apiLambdaName) apiEdgeLambdaInput.name = apiLambdaName;
@@ -312,6 +318,7 @@ class NextjsComponent extends Component {
       code: join(nextConfigPath, DEFAULT_LAMBDA_CODE_DIR),
       memory: getLambdaMemory("defaultLambda"),
       timeout: getLambdaTimeout("defaultLambda")
+      runtime: getLambdaRuntime("defaultLambda")
     };
 
     if (inputs.hasOwnProperty('role_arn')) {
